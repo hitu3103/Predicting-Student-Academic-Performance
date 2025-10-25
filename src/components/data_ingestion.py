7e5b595089d029c057db2a8504c1f9b67c906c76
hitu3103 <hitakshichandan@gmail.com>
2025-10-25 11:06:22 +0530

    test.csv

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,7 @@
 import os
+df = pd.read_csv(os.path.join('notebook', 'data', 'stud.csv'))
 import sys
+from catboost import CatBoostRegressor
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -39,7 +41,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -49,7 +51,7 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__=="__main__":
+if __name__== '__main__':
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
